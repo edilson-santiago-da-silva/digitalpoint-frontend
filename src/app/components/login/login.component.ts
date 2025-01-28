2cef55eef54d64f3e96ee4ae242c42bc3afd62be
edilson-santiago-da-slva <edilsonsantiagoe7@gmail.com>
2025-01-27 21:21:02 -0300

    component login field validation

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { Credentials } from 'src/app/models/credentials';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  creds: Credentials = {
+    user: '',
+    password: ''
+  }
+
+    user = new FormControl(null, Validators.minLength(3));
+    password =  new FormControl(null, Validators.minLength(3))
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+  validFields(): boolean{
+    if(this.user.valid && this.password.valid){
+      return true;
+    } else {
+      return false;
+    }
   }
 
 }
---
+++ src/app/models/credentials.ts
@@ -0,0 +1,4 @@
+export interface Credentials {
+    user: String;
+    password: String;
+}
