a027b3b5c0d44dd98c84e5a7449904ca77b7b469
edilson-santiago-da-slva <edilsonsantiagoe7@gmail.com>
2025-01-31 22:18:40 -0300

    style: component login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -9,6 +9,7 @@ import { AuthService } from 'src/app/services/auth.service';
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
+  
 })
 export class LoginComponent implements OnInit {
 
