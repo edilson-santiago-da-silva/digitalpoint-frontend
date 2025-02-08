11159f30698e5bf9a0af47c2cb2013559aa4e044
edilson-santiago-da-slva <edilsonsantiagoe7@gmail.com>
2025-02-08 12:35:42 -0300

    feat(point): component point findAll

--- src/app/components/point/point.component.ts
+++ src/app/components/point/point.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 import { Point } from 'src/app/models/point';
+import { PointService } from 'src/app/services/point.service';
 
 @Component({
   selector: 'app-point',
@@ -8,61 +11,69 @@ import { Point } from 'src/app/models/point';
 })
 export class PointComponent implements OnInit {
 
-  // Definir as colunas a serem exibidas
-  displayedColumns: string[] = ['dataAtual', 'horarioEntrada', 'horarioSaidaAlmoco', 'horarioEntradaAlmoco', 'horarioSaida', 'minutosExtras'];
-  
-  // Dados fictícios
-  dataSource = [
-    {
-      dataAtual: new Date().toLocaleDateString(),
-      horarioEntrada: '',
-      horarioSaidaAlmoco: '',
-      horarioEntradaAlmoco: '',
-      horarioSaida: '',
-      minutosExtras: 0
-    },
-    // Adicione mais dados aqui conforme necessário
-  ];
+  ELEMENT_DATA: Point[] = []
 
-  constructor() { }
+  // Definir as colunas a serem exibidas
+  displayedColumns: string[] =  ['workDay', 'entry', 'exitLaunch', 'entryLaunch', 'exit', 'minExtra'];
+  // Dados fictícios
+  dataSource = new MatTableDataSource<Point> (this.ELEMENT_DATA);
+
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  constructor(
+    private service: PointService
+  ) { }
 
   ngOnInit(): void {
+    this.findAll();
   }
 
-  // Função para registrar o horário
-  registrarHorario(tipo: string, element: any): void {
-    const now = new Date();
-    const formattedTime = now.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
-
-    // Atribuir o horário correspondente baseado no tipo
-    switch (tipo) {
-      case 'entrada':
-        element.horarioEntrada = formattedTime;
-        break;
-      case 'saidaAlmoco':
-        element.horarioSaidaAlmoco = formattedTime;
-        break;
-      case 'entradaAlmoco':
-        element.horarioEntradaAlmoco = formattedTime;
-        break;
-      case 'saida':
-        element.horarioSaida = formattedTime;
-        break;
-    }
+  findAll(){
+    this.service.findAll().subscribe(Response => {
+      console.log('Dados recebidos:', Response); 
+      this.ELEMENT_DATA = Response
+      this.dataSource = new MatTableDataSource<Point>(Response);
+      this.dataSource.paginator = this.paginator;
+    })
   }
-
-  isButtonAvailable(tipo: string, element: any): boolean {
-      switch (tipo) {
-        case 'entrada':
-          return !element.horarioEntrada;
-        case 'saidaAlmoco':
-          return element.horarioEntrada && !element.horarioSaidaAlmoco;
-        case 'entradaAlmoco':
-          return element.horarioSaidaAlmoco && !element.horarioEntradaAlmoco;
-        case 'saida':
-          return element.horarioEntradaAlmoco && !element.horarioSaida;
-        default:
-          return false;
-      }
+    applyFilter(event: Event): void{
+      const filterValue = (event.target as HTMLInputElement).value;
+      this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 }
+
+  // Função para registrar o horário
+  // registrarHorario(tipo: string, element: any): void {
+  //   const now = new Date();
+  //   const formattedTime = now.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+
+    // Atribuir o horário correspondente baseado no tipo
+//     switch (tipo) {
+//       case 'entrada':
+//         element.horarioEntrada = formattedTime;
+//         break;
+//       case 'saidaAlmoco':
+//         element.horarioSaidaAlmoco = formattedTime;
+//         break;
+//       case 'entradaAlmoco':
+//         element.horarioEntradaAlmoco = formattedTime;
+//         break;
+//       case 'saida':
+//         element.horarioSaida = formattedTime;
+//         break;
+//     }
+//   }
+
+//   isButtonAvailable(tipo: string, element: any): boolean {
+//       switch (tipo) {
+//         case 'entrada':
+//           return !element.horarioEntrada;
+//         case 'saidaAlmoco':
+//           return element.horarioEntrada && !element.horarioSaidaAlmoco;
+//         case 'entradaAlmoco':
+//           return element.horarioSaidaAlmoco && !element.horarioEntradaAlmoco;
+//         case 'saida':
+//           return element.horarioEntradaAlmoco && !element.horarioSaida;
+//         default:
+//           return false;
---
+++ src/app/services/point.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Point } from '../models/point';
+import { API_CONFIG } from '../config/api.config';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PointService {
+
+  constructor(private http: HttpClient) { }
+
+  findAll(): Observable<Point[]> {
+    const token = localStorage.getItem('token'); 
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+
+    return this.http.get<Point[]>(`${API_CONFIG.baseUrl}/points`, {headers});
+  }
+}
